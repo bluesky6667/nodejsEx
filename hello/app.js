4e1c2996872ff7eb624b70685208589bb8466cc4
bluesky6667@gmail.com <cjswoek12>
2016-02-01 09:44:29 +0900

    angularjs

--- hello/app.js
+++ hello/app.js
@@ -27,8 +27,12 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+//app.get('/', routes.index);
 app.get('/users', user.list);
+// 내가 만든 부분
+app.get('/', function(req,res){
+	res.sendfile(__dirname + '/views/index.html');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
